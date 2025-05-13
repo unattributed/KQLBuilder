0b2b3315e54dc7db95e9799d661b258a80a48f46
unattributed <shopkeeper@unattributed.blog>
2025-05-13 13:43:12 -0400

    create SQLiteDialect compatible with Hibernate 6 for SQLite support

--- src/main/java/com/intranet/logquerytool/config/SQLiteDialect.java
+++ src/main/java/com/intranet/logquerytool/config/SQLiteDialect.java
@@ -1,19 +1,30 @@
 package com.intranet.logquerytool.config;
 
+import java.sql.Types;
+
 import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.identity.IdentityColumnSupport;
-import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
-import org.hibernate.engine.jdbc.env.spi.NameQualifierSupport;
+import org.hibernate.dialect.function.StandardAnsiSqlAggregationFunctions;
+import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
+import org.hibernate.type.descriptor.jdbc.IntegerJdbcType;
+import org.hibernate.type.SqlTypes;
 
 public class SQLiteDialect extends Dialect {
 
     public SQLiteDialect() {
         super();
-    }
 
-    @Override
-    public IdentityColumnSupport getIdentityColumnSupport() {
-        return new IdentityColumnSupportImpl();
+        // Register standard types
+        registerColumnType(SqlTypes.INTEGER, "integer");
+        registerColumnType(SqlTypes.VARCHAR, "varchar");
+        registerColumnType(SqlTypes.FLOAT, "float");
+        registerColumnType(SqlTypes.DOUBLE, "double");
+        registerColumnType(SqlTypes.BOOLEAN, "boolean");
+        registerColumnType(SqlTypes.DATE, "date");
+        registerColumnType(SqlTypes.TIMESTAMP, "datetime");
+
+        // Register SQL functions (basic example)
+        this.getFunctionRegistry().register("lower", StandardAnsiSqlAggregationFunctions.LOWER);
+        this.getFunctionRegistry().register("upper", StandardAnsiSqlAggregationFunctions.UPPER);
     }
 
     @Override
@@ -22,8 +33,13 @@ public class SQLiteDialect extends Dialect {
     }
 
     @Override
-    public NameQualifierSupport getNameQualifierSupport() {
-        return NameQualifierSupport.NONE;
+    public boolean supportsInsertReturning() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsTemporaryTables() {
+        return true;
     }
 
     @Override
@@ -31,6 +47,19 @@ public class SQLiteDialect extends Dialect {
         return true;
     }
 
-    // Removed: public boolean supportsCascadeDelete()
+    @Override
+    public boolean supportsCascadeDelete() {
+        return false;
+    }
+
+    @Override
+    public String getIdentityColumnString(int type) {
+        // For SQLite, identity column is always AUTOINCREMENT
+        return "integer";
+    }
+
+    @Override
+    public String getIdentitySelectString(String table, String column, int type) {
+        return "select last_insert_rowid()";
+    }
 }
-// Removed: public boolean supportsCascadeUpdate()
