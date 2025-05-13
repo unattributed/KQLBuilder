7b1235803e7a8bd1966cb7b1f2c8bb1606acac3a
unattributed <shopkeeper@unattributed.blog>
2025-05-13 13:35:48 -0400

    rem unsupported method override from SQLiteDialect for Hibernate 6 compatibilit

--- src/main/java/com/intranet/logquerytool/config/SQLiteDialect.java
+++ src/main/java/com/intranet/logquerytool/config/SQLiteDialect.java
@@ -1,41 +1,19 @@
 package com.intranet.logquerytool.config;
 
-import java.sql.Types;
 import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.function.StandardSQLFunction;
-import org.hibernate.dialect.function.SQLFunctionTemplate;
-import org.hibernate.dialect.function.VarArgsSQLFunction;
-import org.hibernate.type.StringType;
+import org.hibernate.dialect.identity.IdentityColumnSupport;
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
+import org.hibernate.engine.jdbc.env.spi.NameQualifierSupport;
 
 public class SQLiteDialect extends Dialect {
-    public SQLiteDialect() {
-        registerColumnType(Types.BIT, "boolean");
-        registerColumnType(Types.TINYINT, "tinyint");
-        registerColumnType(Types.SMALLINT, "smallint");
-        registerColumnType(Types.INTEGER, "integer");
-        registerColumnType(Types.BIGINT, "bigint");
-        registerColumnType(Types.FLOAT, "float");
-        registerColumnType(Types.REAL, "real");
-        registerColumnType(Types.DOUBLE, "double");
-        registerColumnType(Types.NUMERIC, "numeric");
-        registerColumnType(Types.DECIMAL, "decimal");
-        registerColumnType(Types.CHAR, "char");
-        registerColumnType(Types.VARCHAR, "varchar");
-        registerColumnType(Types.LONGVARCHAR, "longvarchar");
-        registerColumnType(Types.DATE, "date");
-        registerColumnType(Types.TIME, "time");
-        registerColumnType(Types.TIMESTAMP, "timestamp");
-        registerColumnType(Types.BINARY, "blob");
-        registerColumnType(Types.VARBINARY, "blob");
-        registerColumnType(Types.LONGVARBINARY, "blob");
-        registerColumnType(Types.BLOB, "blob");
-        registerColumnType(Types.CLOB, "clob");
-        registerColumnType(Types.BOOLEAN, "boolean");
 
-        registerFunction("concat", new VarArgsSQLFunction(StringType.INSTANCE, "", "||", ""));
-        registerFunction("mod", new SQLFunctionTemplate(StringType.INSTANCE, "?1 % ?2"));
-        registerFunction("substr", new StandardSQLFunction("substr", StringType.INSTANCE));
-        registerFunction("substring", new StandardSQLFunction("substr", StringType.INSTANCE));
+    public SQLiteDialect() {
+        super();
+    }
+
+    @Override
+    public IdentityColumnSupport getIdentityColumnSupport() {
+        return new IdentityColumnSupportImpl();
     }
 
     @Override
@@ -44,103 +22,8 @@ public class SQLiteDialect extends Dialect {
     }
 
     @Override
-    public boolean hasDataTypeInIdentityColumn() {
-        return false;
-    }
-
-    @Override
-    public String getIdentityColumnString() {
-        return "integer";
-    }
-
-    @Override
-    public String getIdentitySelectString() {
-        return "select last_insert_rowid()";
-    }
-
-    @Override
-    public boolean supportsLimit() {
-        return true;
-    }
-
-    @Override
-    public String getLimitString(String query, boolean hasOffset) {
-        return query + (hasOffset ? " limit ? offset ?" : " limit ?");
-    }
-
-    @Override
-    public boolean supportsTemporaryTables() {
-        return true;
-    }
-
-    @Override
-    public String getCreateTemporaryTableString() {
-        return "create temporary table if not exists";
-    }
-
-    @Override
-    public boolean dropTemporaryTableAfterUse() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsCurrentTimestampSelection() {
-        return true;
-    }
-
-    @Override
-    public boolean isCurrentTimestampSelectStringCallable() {
-        return false;
-    }
-
-    @Override
-    public String getCurrentTimestampSelectString() {
-        return "select current_timestamp";
-    }
-
-    @Override
-    public boolean supportsUnionAll() {
-        return true;
-    }
-
-    @Override
-    public boolean hasAlterTable() {
-        return false;
-    }
-
-    @Override
-    public boolean dropConstraints() {
-        return false;
-    }
-
-    @Override
-    public String getAddColumnString() {
-        return "add column";
-    }
-
-    @Override
-    public String getForUpdateString() {
-        return "";
-    }
-
-    @Override
-    public boolean supportsOuterJoinForUpdate() {
-        return false;
-    }
-
-    @Override
-    public String getDropForeignKeyString() {
-        return "";
-    }
-
-    @Override
-    public String getAddForeignKeyConstraintString(String cn, String[] fk, String t, String[] pk, boolean rpk) {
-        return "";
-    }
-
-    @Override
-    public String getAddPrimaryKeyConstraintString(String constraintName) {
-        return "";
+    public NameQualifierSupport getNameQualifierSupport() {
+        return NameQualifierSupport.NONE;
     }
 
     @Override
@@ -148,8 +31,6 @@ public class SQLiteDialect extends Dialect {
         return true;
     }
 
-    @Override
-    public boolean supportsCascadeDelete() {
-        return false;
-    }
+    // Removed: public boolean supportsCascadeDelete()
 }
+// Removed: public boolean supportsCascadeUpdate()
