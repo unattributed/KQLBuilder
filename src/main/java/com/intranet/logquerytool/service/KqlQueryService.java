62d302802ed4c1de5c950407b89efed1ee1b3870
unattributed <shopkeeper@unattributed.blog>
2025-05-13 12:59:23 -0400

    chg  for standard in Spring Data patterns (save, findById, findAll).

--- src/main/java/com/intranet/logquerytool/service/KqlQueryService.java
+++ src/main/java/com/intranet/logquerytool/service/KqlQueryService.java
@@ -2,9 +2,9 @@ package com.intranet.logquerytool.service;
 
 import com.intranet.logquerytool.model.KqlQuery;
 import com.intranet.logquerytool.repository.KqlQueryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -12,40 +12,20 @@ public class KqlQueryService {
 
     private final KqlQueryRepository repository;
 
+    @Autowired
     public KqlQueryService(KqlQueryRepository repository) {
         this.repository = repository;
     }
 
-    /**
-     * Save a new KQL query to the database.
-     * @param query the KqlQuery object to save
-     */
-    public void save(KqlQuery query) {
-        repository.save(query);
+    public KqlQuery save(KqlQuery query) {
+        return repository.save(query);
     }
 
-    /**
-     * Get a specific query by ID.
-     * @param id the ID of the query
-     * @return the KqlQuery object if found
-     */
-    public Optional<KqlQuery> getById(Long id) {
+    public Optional<KqlQuery> findById(Long id) {
         return repository.findById(id);
     }
 
-    /**
-     * Retrieve all saved KQL queries.
-     * @return a list of all queries
-     */
-    public List<KqlQuery> getAllQueries() {
-        return (List<KqlQuery>) repository.findAll();
-    }
-
-    /**
-     * Delete a query by ID.
-     * @param id the ID of the query to delete
-     */
-    public void deleteById(Long id) {
-        repository.deleteById(id);
+    public Iterable<KqlQuery> findAll() {
+        return repository.findAll();
     }
 }
