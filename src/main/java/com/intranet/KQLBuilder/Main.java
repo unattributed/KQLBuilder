70ac1c7b36f6218a18a7b1cb721d58423e3761be
unattributed <shopkeeper@unattributed.blog>
2025-05-13 06:17:15 -0400

    fix pom for mvn build issue, added Main.java, built KqlBuilderApplication.java and upd applicaton.yml

---
+++ src/main/java/com/intranet/KQLBuilder/Main.java
@@ -0,0 +1,7 @@
+package com.intranet.KQLBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("KQLBuilder is running!");
+    }
+}
---
+++ src/main/java/com/intranet/logquerytool/KqlBuilderApplication.java
@@ -0,0 +1,15 @@
+package com.intranet.logquerytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the KQLBuilder Spring Boot application.
+ */
+@SpringBootApplication
+public class KqlBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KqlBuilderApplication.class, args);
+    }
+}
