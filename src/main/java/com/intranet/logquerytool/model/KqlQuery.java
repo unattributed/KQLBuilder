b384aba5c1c503c96daceebefa632bdee3514e1a
unattributed <shopkeeper@unattributed.blog>
2025-05-14 15:16:35 -0400

    Add getters and setters for KqlQuery fields - Implemented getter and setter methods for all fields in the KqlQuery class. - Ensures encapsulation and allows controlled access to class properties.

--- src/main/java/com/intranet/logquerytool/model/KqlQuery.java
+++ src/main/java/com/intranet/logquerytool/model/KqlQuery.java
@@ -1,46 +1,96 @@
 package com.intranet.logquerytool.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Access(AccessType.FIELD)
+@Table(name = "kql_queries")
 public class KqlQuery {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String cloud;
-    private String logType;
-    private String query;
+    private String cloudProvider;
+    private String logSource;
+    private String table;
+    private String field;
+    private String operator;
+    private String value;
+    private String generatedQuery;
 
     public KqlQuery() {}
 
-    public KqlQuery(String cloud, String logType, String query) {
-        this.cloud = cloud;
-        this.logType = logType;
-        this.query = query;
+    public KqlQuery(String cloudProvider, String logSource, String table, String field, String operator, String value, String generatedQuery) {
+        this.cloudProvider = cloudProvider;
+        this.logSource = logSource;
+        this.table = table;
+        this.field = field;
+        this.operator = operator;
+        this.value = value;
+        this.generatedQuery = generatedQuery;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getCloud() { return cloud; }
-    public void setCloud(String cloud) { this.cloud = cloud; }
+    public String getCloudProvider() {
+        return cloudProvider;
+    }
 
-    public String getLogType() { return logType; }
-    public void setLogType(String logType) { this.logType = logType; }
+    public void setCloudProvider(String cloudProvider) {
+        this.cloudProvider = cloudProvider;
+    }
 
-    public String getQuery() { return query; }
-    public void setQuery(String query) { this.query = query; }
+    public String getLogSource() {
+        return logSource;
+    }
 
-    @Override
-    public String toString() {
-        return "KqlQuery{" +
-               "id=" + id +
-               ", cloud='" + cloud + '\'' +
-               ", logType='" + logType + '\'' +
-               ", query='" + query + '\'' +
-               '}';
+    public void setLogSource(String logSource) {
+        this.logSource = logSource;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getGeneratedQuery() {
+        return generatedQuery;
+    }
+
+    public void setGeneratedQuery(String generatedQuery) {
+        this.generatedQuery = generatedQuery;
     }
 }
